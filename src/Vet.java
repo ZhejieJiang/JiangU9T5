2c1fcc3ea55bd5bf999ea1508097d313e54760da
unknown <BT_3W2_06@BKTECH.LOCAL>
2023-03-29 09:32:10 -0400

    Initial commit

---
+++ src/Vet.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+public class Vet
+{
+    private ArrayList<Animal> client;
+    private String name;
+
+    public Vet(String name)
+    {
+        this.name = name;
+        client = new ArrayList<Animal>();
+    }
+
+    public void addClient(Animal x)
+    {
+        if(!client.contains(x)){
+        client.add(x);
+        System.out.println("Welcome to " + name + " office," + x.getName()+ "!");
+        }
+        else
+        {
+            
+        }
+    }
+}
